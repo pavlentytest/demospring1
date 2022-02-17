7b48e6545f7939bd7eecc1d5df8c31200b5c65f4
Pavel <impetus@yandex.ru>
2022-02-17 11:30:44 +0300

    Remove imports

--- src/main/java/ru/pavlenty/demospring1/MainController.java
+++ src/main/java/ru/pavlenty/demospring1/MainController.java
@@ -1,7 +1,6 @@
 package ru.pavlenty.demospring1;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
